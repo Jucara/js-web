e0a8a124ac1120878729b5b6c9f16746732d053f
Julien Caramazza <julien.caramazza@gmail.com>
2019-01-28 11:59:07 +0100

    fin ex1.5

--- ex1-5bis/script.js
+++ ex1-5bis/script.js
@@ -11,3 +11,31 @@ import "./style.scss";
 /*
   Put the JavaScript code you want below.
 */
+import img1 from './980x.jpg';
+import img2 from './38588p.jpg';
+import img3 from './cat1.jpg';
+import img4 from './cat2.jpg';
+import img5 from './cat4.jpg';
+import img6 from './cat12.jpg';
+import img7 from './cat123.jpg';
+import img8 from './cat124.jpg';
+import img9 from './cath1.png';
+import img10 from './index.jpeg';
+import img11 from './lead411.jpg';
+import img12 from './maxresdefault.jpg';
+
+var indexphoto = 0;
+phototurn ();
+
+function phototurn(){
+  var i;
+  var x= document.getElementsByClassName("photo");
+  for (i=0;i<x.length;i++){
+    x[i].style.display="none";
+  }
+  indexphoto++;
+  if (indexphoto > x.length)
+  {indexphoto = 1}
+  x[indexphoto-1].style.display = "block";
+  setTimeout(phototurn,1000);
+}
